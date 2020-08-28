603d0b5c0bd6ffdec5ed60557d561a24cf786b3c
emerykurt <emerylumsden@gmail.com>
2020-08-28 12:18:02 -0400

    review form functional

--- frontend/src/functions.js
+++ frontend/src/functions.js
@@ -40,6 +40,8 @@ const handleClickEvent = (e) => {
     // debugger
     if (e.target.id.includes('company-info')){
         writeReview()
+    } else if (e.target.id === "sign-up-submit"){
+        submitResults(e)
     } else if(e.target.id.includes('moreInfoCheckBox-')){
         toggleMoreInfo(e.target) 
     } else if(e.target.id.includes('https://')){
@@ -61,20 +63,41 @@ const handleClickEvent = (e) => {
     }
 }//working
 
-const toggleMoreInfo = (info) => {
+const toggleHired = (e) => {
+    if (e.checked === true){
+        const interviewExtenArea = document.getElementById("interviewExt")
+        const interviewExtForm = `
+        <label for="interview">Interview Process:</label>
+        <select name="interviewProcess" id="interviewProcess" form="company-review">
+            <option value=" "> </option>
+            <option value="easy">easy</option>
+            <option value="moderate">moderate</option>
+            <option value="extensive">extensive</option>
+        </select><br/>
+        <label for="hired">Were you hired?</label>
+        <input type="checkbox" name="hired" id="hiredCheckBox"><br/>
+        <div id="hiredExt" form="hired-portion">
+        </div>`
+        interviewExtenArea.innerHTML = interviewExtForm
+    } else if (e.checked === false){
+        const interviewExtenArea = document.getElementById("interviewExt")
+        interviewExtenArea.innerHTML = " " 
+    }
+}
+const toggleMoreInfo = (e) => {
     // debugger
-    if (info.checked === true){
+    if (e.checked === true){
         // debugger
-        const moreInfoArea = document.getElementById(`element-${info.id}`)
+        const moreInfoArea = document.getElementById(`element-${e.id}`)
         moreInfoArea.style.display = 'block'
-        const moreInfoLabel = document.getElementById(`moreInfoLabel-${info.id}`)
+        const moreInfoLabel = document.getElementById(`moreInfoLabel-${e.id}`)
         moreInfoLabel.innerHTML = "less..."
     }
-    else if (info.checked === false){
+    else if (e.checked === false){
         // debugger
-        const moreInfoArea = document.getElementById(`element-${info.id}`)
+        const moreInfoArea = document.getElementById(`element-${e.id}`)
         moreInfoArea.style.display = 'none'
-        const moreInfoLabel = document.getElementById(`moreInfoLabel-${info.id}`)
+        const moreInfoLabel = document.getElementById(`moreInfoLabel-${e.id}`)
         moreInfoLabel.innerHTML = "more..."
     }
 }
@@ -91,20 +114,56 @@ const twitterLink = (info) => {
 
 const writeReview = () => {
     const reviewForm = `<form id="company-review">
+    <label for="company-id">Company:</label>
+    <select name="companyId" id="companyId" form="company-review">
+        <option value=" "> </option>
+        <option value="1">HubSpot</option>
+        <option value="2">DocuSign</option>
+        <option value="3">Ultimate Software</option>
+        <option value="4">Google</option>
+        <option value="5">LinkedIn</option>
+        <option value="6">MathWorks</option>
+        <option value="7">Nvidia</option>
+        <option value="8">Microsoft</option>
+        <option value="9">Facebook</option>
+        <option value="10">Compass</option>
+        <option value="11">Survey Monkey</option>
+        <option value="12">Saleforce</option>
+        <option value="13">Kronos Incorporated</option>
+        <option value="14">VMware</option>
+        <option value="15">Adobe</option>
+        <option value="16">Appfolio</option>
+        <option value="17">SAP</option>
+        <option value="18">CDW</option>
+        <option value="19">Yardi Systems</option>
+        <option value="20">JDA Software (Blue Yonder)</option>
+        <option value="21">Nextiva</option>
+        <option value="22">Paycom</option>
+        <option value="23">Dell</option>
+        <option value="24">Slack</option>
+        <option value="25">Intuit</option>
+        <option value="26">Noon</option>
+        <option value="27">UST Global</option>
+        <option value="28">Cisco Systems</option>
+        <option value="29">Apple</option>
+        <option value="30">Epic Systems</option>
+        <option value="31">Intel</option>
+    </select><br/>
     <label for="interview">Did you interview?</label>
     <input type="checkbox" name="Interview" id="interviewCheckBox"><br/>
         <div id="interviewExt" form="interview-portion">
         </div>
     <label for="firstname">First Name:</label>
-    <input type="text" name="firstName"><br/>
+    <input type="text" name="firstName" id="firstName><br/>
     <label for="last_name">Last Name:</label>
-    <input type="text" name="last_name" id="last_name"><br/>
+    <input type="text" name="last_name" id="lastName"><br/>
     <label for="city">City:</label>
     <input type="text" name="city" id="city"><br/>
     <label for="state">State:</label>
     <input type="text" name="state" id="state"><br/>
     <label for="bootcamp">What bootcamp did you go to?</label>
-    <select name="bootcamp" id="bootcamp" form="signUpForm">
+    <select name="bootcamp" id="bootcamp" form="company-review">
+        <option value=" "> </option>
         <option value="FlatIron">FlatIron</option>
         <option value="General Assembly">General Assembly</option>
         <option value="Kenzie Academy">Kenzie Academy</option>
@@ -122,14 +181,19 @@ const writeReview = () => {
     <input type="submit" value="Submit Review" id="sign-up-submit">
     </form>` 
     enterArea.innerHTML = reviewForm
-} // working
+} 
 
 function renderInterviewExtension(){
     // debugger
     const interviewExtenArea = document.getElementById("interviewExt")
     const interviewExtForm = `
     <label for="interview">Interview Process:</label>
-    <input type="text" name="Interview Process" id="interview-process"><br/>
+    <select name="interviewProcess" id="interviewProcess" form="company-review">
+        <option value=" "> </option>
+        <option value="easy">easy</option>
+        <option value="moderate">moderate</option>
+        <option value="extensive">extensive</option>
+    </select><br/>
     <label for="hired">Were you hired?</label>
     <input type="checkbox" name="hired" id="hiredCheckBox"><br/>
         <div id="hiredExt" form="hired-portion">
@@ -146,13 +210,29 @@ function renderHiredExtension(){
     const hiredExtenArea = document.getElementById("hiredExt")
     const hiredExtForm = `
     <label for="company-lifestyle">Company Lifestyle:</label>
-    <input type="text" name="Company Lifestyle" id="company-lifestyle"><br/>
+    <select name="companyLifestyle" id="companyLifestyle" form="signUpForm">
+        <option value=" "> </option>
+        <option value="innovative">innovative</option>
+        <option value="challenging">challenging</option>
+        <option value="progressive">progressive</option>
+        <option value="micromanaged">micromanaged</option>
+        <option value="flexible">flexible</option>
+        <option value="problematic">problematic</option>
+    </select><br/>
     <label for="compensation">Compensation (this information will not be displayed or connected with your name):</label>
-    <input type="number" min="50000.00" step="500.00" name="compensation" id="compensation"><br/>
+    <input type="number" min="50000.00" step="5.00" name="compensation" id="compensation"><br/>
     <label for="management-mentorship">Management/Mentorship:</label>
-    <input type="text" name="Management Mentorship" id="management-mentorship"><br/>
+    <select name="managementMentorship" id="managementMentorship" form="signUpForm">
+        <option value=" "> </option>
+        <option value="yes">yes</option>
+        <option value="no">no</option>
+    </select><br/>
     <label for="diversity">Diversity:</label>
-    <input type="text" name="Diversity" id="diversity"><br/>`
+    <select name="diversity" id="diversity" form="signUpForm">
+    <option value=" "> </option>
+    <option value="yes">yes</option>
+    <option value="no">no</option>
+    </select><br/>`
     hiredExtenArea.innerHTML = hiredExtForm
 }
 
@@ -172,6 +252,20 @@ function renderQuiz(){
 function buildQuiz(){
     const myQuestions = []
 }
-function submitResults(){
-
+function submitResults(e){
+    debugger
+    console.log("we are here")
+    const interview = document.getElementById("interview")
+    const interviewProcess = document.getElementById("interviewProcess")
+    const hired = document.getElementById("hiredCheckBox")
+    const companyLifestyle = document.getElementById("companyLifestyle")
+    const compensation = document.getElementById("compensation")
+    const managementMentorship = document.getElementById("managementMentorship")
+    const diversity = document.getElementById("diversity") 
+    const firstName = document.getElementById("firstName")
+    const lastName = document.getElementById("lastName")
+    const bootcamp = document.getElementById("bootcamp")
+    const city = document.getElementById("city")
+    const state = document.getElementById("state")
+    const companyId = document.getElementById("companyId")
 }
